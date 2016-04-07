92892a8347566555b327773fd2c1aa49b4dca13a
Heli Pykälä <heli.pykala@iki.fi>
2016-04-07 10:14:14 +0300

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions makeCacheMatrix and cacheSolve make it possible to cache 
+## the inverse of a matrix to save time with computations.
+## Use makeCacheMatrix to create a cachable matrix and
+## cacheSolve to solve and cache its inverse.
 
-## Write a short comment describing this function
 
+## Create a cacheable matrix to save time inverting the matrix
 makeCacheMatrix <- function(x = matrix()) {
+    # cached value is set as null as it's not calculated yet
     inv <- NULL
+    
+    # if a new value for matrix is set, null the inverse
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+    
+    # get the value of matrix
     get <- function() x
+    
+    # set inverse
     setinv <- function(inverse) inv <<- inverse
+    
+    # get inverse
     getinv <- function() inv
+    
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
@@ -19,17 +31,29 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is inverse of x. 
+##
+## X must be a CacheMatrix created by makeCacheMatrix function
+## If the inverse matrix has been already calculated cacheSolve returns the cached value along with message 
+## "getting cached data"
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+
+    # check if the inverse has been cached and return the cache value if it exists
     inv <- x$getinv()
     if(!is.null(inv)) {
         message("getting cached data")
         return(inv)
     }
+    
+    # If there's no cached inverse get the CacheMAtrix data, solve its inverse, and store the result
     data <- x$get()
+    
+    # no error handling since we can assume the matrix is always invertible. 
+    # If this is not the case solve will throw an error
     inv <- solve(data, ...)
+    
+    # save the inverse to the CacheMatrix so it can be reused
     x$setinv(inv)
+    
     inv
 }
